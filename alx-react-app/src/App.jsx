9aef3f30ed08e847f53f71f596d16860f1714f64
KEVIN NSHUTI <nkevin881@gmail.com>
2025-07-13 19:41:19 +0200

    adding more components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,12 +3,17 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import WelcomeMessage from "./components/WelcomeMessage";
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MainContent from "./components/MainContent";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <Header />
+      <MainContent />
+      <Footer />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
