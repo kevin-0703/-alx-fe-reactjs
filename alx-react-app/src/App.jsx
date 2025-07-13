bc5d42838be89bcdffa062e1b6db5d6b6dd19b7c
KEVIN NSHUTI <nkevin881@gmail.com>
2025-07-13 20:03:02 +0200

    adding more components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,8 @@ import WelcomeMessage from "./components/WelcomeMessage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MainContent from "./components/MainContent";
+import React from "react";
+import UserProfile from "./components/UserProfile";
 function App() {
   const [count, setCount] = useState(0);
 
@@ -14,6 +16,16 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+
+      function App() {
+        return (
+          <div className="App">
+            <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+          </div>
+        );
+      }
+
+      export default App;
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
