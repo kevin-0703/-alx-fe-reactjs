7679d5055e39e8c73e5a298cee40bb58ae8ce478
KEVIN NSHUTI <nkevin881@gmail.com>
2025-07-17 17:32:42 +0200

    adding a customized counter

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -8,36 +8,15 @@ import Footer from "./components/Footer";
 import MainContent from "./components/MainContent";
 import React from "react";
 import UserProfile from "./components/UserProfile";
+import counter from "./components/counter";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Header />
       <MainContent />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <counter />
       <Footer />
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <WelcomeMessage />
     </>
   );
 }
