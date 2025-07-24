98db2ffe7600fdd9e2c724e34560f4ded39c0e42
KEVIN NSHUTI <nkevin881@gmail.com>
2025-07-24 13:54:09 +0200

    creating a recipe sharing app

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import RecipeList from "./components/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Recipe Manager</h1>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+  );
+};
---
+++ recipe-sharing-app/src/stores/recipeStore.js
@@ -0,0 +1,6 @@
+import { create } from 'zustand';
+const useRecipeStore = create(set => ({
+    recipes: [],
+    addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] }))
+}));
+export default useRecipeStore;
