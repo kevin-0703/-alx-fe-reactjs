8049cd1861b36e3e9358e1a1c6804e045a225694
KEVIN NSHUTI <nkevin881@gmail.com>
2025-07-29 20:41:35 +0200

    initial project setup

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <h1>Github search</h1>
+      <p>Start typing github name for searching</p>
+    </>
+  );
+}
+
+export default App;
