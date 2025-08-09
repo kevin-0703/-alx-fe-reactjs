938b1ae4806df069131d6450fc37e76577f24426
KEVIN NSHUTI <nkevin881@gmail.com>
2025-08-09 19:03:30 +0200

    making responsive design

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="bg-gray-100 md:p-8 md:max-w-sm sm:max-w-xs sm:p-4 mx-auto my-20 rounded-lg shadow-lg">
       <img
-        className="rounded-full w-36 h-36 mx-auto"
+        className="rounded-full md:w-36 md:h-36 mx-auto sm:w-24 sm:h-24"
         src="https://via.placeholder.com/150"
         alt="User"
       />
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-gray-600 text-base">
+      <h1 className="md:text-xl text-blue-800 my-4 sm:text-lg">John Doe</h1>
+      <p className="text-gray-600 md:text-base sm:text-sm">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
