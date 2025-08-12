ce32b95a03bbb1ce6abe6f26b9213ef223eff38b
KEVIN NSHUTI <nkevin881@gmail.com>
2025-08-12 20:14:04 +0200

    project setup

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './index.html',
+        './src/**/*.{js,jsx,ts,tsx}',
+        './public/index.html'
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
