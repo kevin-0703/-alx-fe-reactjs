020e5ac4d8cbe8ec9053028fe2ba1610325f652b
KEVIN NSHUTI <nkevin881@gmail.com>
2025-08-13 16:13:39 +0200

    adding a form

--- recipe-sharing-platform/src/components/AddRecipeForm.jsx
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -13,7 +13,7 @@ function AddRecipeForm() {
       summary,
       image,
       ingredients: ingredients.split(",").map((item) => item.trim()),
-      instructions: instructions.split(",").map((item) => item.trim()),
+      instructions: instructions.split(",").map((step) => item.trim()),
     };
   };
   return (
